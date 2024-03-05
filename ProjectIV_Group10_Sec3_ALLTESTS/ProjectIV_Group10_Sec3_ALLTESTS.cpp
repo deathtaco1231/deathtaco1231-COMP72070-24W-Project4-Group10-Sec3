e81c1a8b1ef05cf25bccd3e20c2453df94da81bc
Julian Cumming <julianjcumming@gmail.com>
2024-03-05 15:27:27 -0500

    Test Project Created

---
+++ ProjectIV_Group10_Sec3_ALLTESTS/ProjectIV_Group10_Sec3_ALLTESTS.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ProjectIVGroup10Sec3ALLTESTS
+{
+	TEST_CLASS(ProjectIVGroup10Sec3ALLTESTS)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
