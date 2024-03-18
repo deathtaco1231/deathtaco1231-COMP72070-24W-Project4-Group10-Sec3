169c80f7ec10b8d0b190c12207f0277872b81cf0
Julian Cumming <julianjcumming@gmail.com>
2024-03-18 12:46:30 -0400

    Added Test Projects

---
+++ AidanNickersonTestProj/AidanNickersonTestProj.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace AidanNickersonTestProj
+{
+	TEST_CLASS(AidanNickersonTestProj)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ JulianTestProj/JulianTestProj.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace JulianTestProj
+{
+	TEST_CLASS(JulianTestProj)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
